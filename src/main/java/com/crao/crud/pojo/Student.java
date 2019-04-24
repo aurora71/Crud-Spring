a9dd5e6efdbe5b1746fba3bea73f16b6185d422e
aurora71 <cr2832@columbia.edu>
2019-04-24 12:44:23 -0400

    Add files via upload

---
+++ src/main/java/com/crao/crud/pojo/Student.java
@@ -0,0 +1,33 @@
+package com.crao.crud.pojo;
+
+public class Student {
+    private Long id;
+
+    private String name;
+
+    private String major;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+}
