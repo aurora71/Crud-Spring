53aa8b38d077fd5a74c9ec6f29625993b4995cf2
aurora71 <cr2832@columbia.edu>
2019-04-24 12:45:03 -0400

    Update StudentController.java

--- src/main/java/com/crao/crud/controller/StudentController.java
+++ src/main/java/com/crao/crud/controller/StudentController.java
@@ -1,12 +1,14 @@
 package com.crao.crud.controller;
 
-import com.crao.crud.dto.Student;
+import com.crao.crud.dto.StudentDTO;
+import com.crao.crud.pojo.Student;
 import com.crao.crud.service.RedisService;
 import com.crao.crud.service.StudentService;
 import com.github.pagehelper.Page;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,15 +31,16 @@ public class StudentController {
     }
 
     @RequestMapping(value = "/getAll", method = RequestMethod.GET)
-    public Page<Student> testList(Student student){
+    public Page<Student> testList(StudentDTO student){
         Page<Student> test = studentService.getTestList(student);
         System.out.println(test);
         return test;
     }
 
+
     @RequestMapping(value = "/getStudentByID", method = RequestMethod.GET)
-    public Page<Student> getStudent(Student student){
-        Page<Student> test=studentService.getStudent(student);
+    public Page<Student> getStudent(Student dto){
+        Page<Student> test=studentService.getStudent(dto.getId());
         return test;
     }
 
@@ -57,5 +60,17 @@ public class StudentController {
     public void updateStudent(Student student){
         studentService.updateStudent(student);
         return;
+    }
+
+    @RequestMapping(value = "/getDelStudent", method = RequestMethod.GET)
+    public Page<Student> getDelStudent(Student student) {
+        Page<Student> test = studentService.getDelStudent();
+        return test;
+    }
+
+    @GetMapping("/userPage")
+    public Page<Student> queryPageStudent(StudentDTO student) {
+        Page<Student> test = studentService.queryPageStudent(student);
+        return test;
     }
 }
