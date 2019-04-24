997d76b79d765227ce1a62fdf3183d9246926bdc
aurora71 <cr2832@columbia.edu>
2019-04-24 12:45:28 -0400

    Update StudentService.java

--- src/main/java/com/crao/crud/service/StudentService.java
+++ src/main/java/com/crao/crud/service/StudentService.java
@@ -1,16 +1,18 @@
 package com.crao.crud.service;
 
 import com.crao.crud.dao.StudentMapper;
-import com.crao.crud.dto.Student;
+import com.crao.crud.dto.StudentDTO;
+import com.crao.crud.pojo.Student;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 public class StudentService {
 
-    @Autowired
+    @Resource
     private StudentMapper studentMapper;
     /**
      *
@@ -20,11 +22,11 @@ public class StudentService {
         return studentMapper.getTest();
     }
 
-    public Page<Student> getStudent(Student student){
-        return studentMapper.getStudent(student);
+    public Page<Student> getStudent(long id){
+        return studentMapper.getStudent(id);
     }
 
-    public Page<Student> getTestList(Student student){
+    public Page<Student> getTestList(StudentDTO student){
         PageHelper.orderBy(student.getOrderBy());
         System.out.println(student.getOrderBy());
         return studentMapper.getTestList(student);
@@ -40,5 +42,15 @@ public class StudentService {
 
     public void updateStudent(Student student) {
         studentMapper.updateStudent(student);
+    }
+
+    public Page<Student> getDelStudent() {
+        return studentMapper.getDelStudent();
+    }
+
+    public Page<Student> queryPageStudent(StudentDTO student) {
+        PageHelper.startPage(student.getPageNum(),student.getPageSize());
+        PageHelper.orderBy(student.getOrderBy());
+        return studentMapper.getTestList(student);
     }
 }
