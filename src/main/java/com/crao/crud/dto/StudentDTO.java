58fb0536af33de7b040c51fdbe2341d3166bd31c
aurora71 <cr2832@columbia.edu>
2019-04-24 12:43:55 -0400

    Add files via upload

---
+++ src/main/java/com/crao/crud/dto/StudentDTO.java
@@ -0,0 +1,38 @@
+package com.crao.crud.dto;
+
+import com.crao.crud.common.baseDto;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(Include.NON_NULL)
+public class StudentDTO extends baseDto {
+    private Long id;
+
+    private String name;
+
+    private String major;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+}
